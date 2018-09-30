d7d7f684a3c07c4a598a3aa679f6d2e7f06bbe55
Spitfyre03 <spitfyre03@gmail.com>
2018-09-30 17:06:01 -0400

    Refactored setup for adding cable
    Cable form divs are now all stored in a wrapping div for displaying the focused pane. The active cable section is named "new-cable".

--- public_html/js/cable_builder.js
+++ public_html/js/cable_builder.js
@@ -1,17 +1,37 @@
-function addCable() {
-    var body = document.getElementById("cable-builder").getElementsByTagName("tbody")[0];
-    var index = body.rows.length;
-    var cable = cookieArrayGet('cable');
-    if (cable === [] || cable[0] !== index) {
-        alert("You must complete the current cable first");
-        return;
+
+function cloneTemplate(element) {
+    var clone = element.cloneNode(true);
+    var index = cookieArrayGet("cable");
+    clone.setAttribute("id", element.id.replace("template-","").concat("-" + index).trim());
+    return clone;
+}
+
+function initSetup() {
+    if (addCable()) {
+        var clone = cloneTemplate(document.getElementById("template-cable-setup"));
+        document.getElementById("new-cable").appendChild(clone);
+        document.getElementById("cables-form").classList.remove("hide");
     }
+}
+
+function addCable() {
+    var body = document.getElementById("quote-items-body");
+    var index = body.rows.length;
+    var div = document.getElementById("new-cable");
+    if (div != null) {
+        alert("You must complete the current cable first");
+        return false;
+    }
+    var display = document.createElement("div");
+    display.id = "new-cable";
+    document.getElementById("form-panel").appendChild(display);
     var cell = document.createElement("td");
-    cell.innerHTML = "Cable " + index;
+    cell.innerText = "Cable " + index;
     var row = document.createElement("tr");
     row.id = "quote-item-" + index;
     row.appendChild(cell);
-    var bRow = document.getElementById("cable-adder");
+    var bRow = document.getElementById("table-cable-adder");
     bRow.insertAdjacentElement("beforebegin",row);
-    document.getElementsByTagName("form")[0].classList.remove("hide");
+    return true;
+}
 }
