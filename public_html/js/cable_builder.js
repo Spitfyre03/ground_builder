038bb47e4f75af54f031434a7243d344b486bad2
Spitfyre03 <spitfyre03@gmail.com>
2018-10-01 11:17:02 -0400

    Add transition logic and search setup
    Added methods for dynamically arranging search panes in an order appropriate for the cable style. Can traverse both forward and backward between panes.
    Search panes currently have no purpose. Database match table to be added. No handling yet for what fields are required in the form submission

--- public_html/js/cable_builder.js
+++ public_html/js/cable_builder.js
@@ -6,7 +6,7 @@ function cloneTemplate(element) {
     return clone;
 }
 
-function initSetup() {
+function startCable() {
     if (addCable()) {
         var clone = cloneTemplate(document.getElementById("template-cable-setup"));
         document.getElementById("new-cable").appendChild(clone);
@@ -34,4 +34,110 @@ function addCable() {
     bRow.insertAdjacentElement("beforebegin",row);
     return true;
 }
+
+function setupCable(container) {
+    var type = container.querySelector("select").value;
+    createCableTree(type);
+    setupCableForm(type).forEach(function(child) {
+        container.parentElement.appendChild(child);
+    });
+    next(container);
+}
+
+/** Initiates a tree for displaying the cable in the review table */
+function createCableTree(type) {
+    var isCluster = type > 1;
+    var itt = (isCluster ? 4 : 1);
+    var index = document.getElementById("quote-items-body").getElementsByTagName("tr").length - 1;
+    var cell = document.getElementById("quote-item-" + index).children[index - 1];
+    if (cell.querySelector("ul") !== null) {
+        cell.removeChild(cell.lastChild);
+    }
+    var tree = document.createElement("ul");
+    var id = 'a';
+    var num = 1;
+    for (var x = 0; x < itt; x++) {
+        var cable = document.createElement("li");
+        cable.id = "cable-" + id;
+        cable.classList.add("hide");
+        for (var y = 0; y < 2; y++) {  
+            var ferrList = document.createElement("ul");
+            var ferr = document.createElement("li");
+            ferr.id = "ferrule-" + num;
+            ferr.classList.add("hide");
+            var cabList = document.createElement("ul");
+            var shrink = document.createElement("li");
+            shrink.innerText = "Shrink";
+            cabList.appendChild(shrink);
+            if (isCluster && y != 1) {
+                var clamp = document.createElement("li");
+                clamp.id = "clamp-" + num;
+                clamp.classList.add("hide");
+                cabList.appendChild(clamp);
+            }
+            ferr.appendChild(cabList);
+            ferrList.appendChild(ferr);
+            cable.appendChild(ferrList);
+            num++;
+        }
+        id = String.fromCharCode(id.charCodeAt() + 1);
+        tree.appendChild(cable);
+    }
+    cell.appendChild(tree);
+}
+
+function setupCableForm(type) {
+    var isCluster = type > 1;
+    var arr = isCluster ? new Array(10) : new Array(5);
+    if (isCluster) {
+        // Cable, modified for cluster style
+        var cable = cloneTemplate(document.getElementById("template-cable-search"));
+        var quant = cable.children.namedItem("cable-length-div");
+        var button = quant.nextElementSibling;
+        Array.from(document.getElementById("cluster-lengths").children).forEach(function(e){
+            button.insertAdjacentElement("beforebegin",e);
+        });
+        quant.remove();
+        arr.push(cable);
+        // Block Ferrule
+        var blockFerr = cloneTemplate(document.getElementById("template-ferrule-search"));
+        blockFerr.firstElementChild.remove();
+        var title = document.createElement("h2");
+        title.classList.add("centered");
+        title.innerText = "Block Ferrules";
+        blockFerr.insertAdjacentElement("afterbegin",title);
+        blockFerr.id = blockFerr.id.concat("-block");
+        arr.push(blockFerr);
+        // Ferrules 1-4, Clamps 1-4 in between
+        for (var i = 1; i <= 4; i++) {
+            var ferr = cloneTemplate(document.getElementById("template-ferrule-search"));
+            ferr.id = ferr.id.concat("-" + i);
+            arr.push(ferr);
+            var clamp = cloneTemplate(document.getElementById("template-clamp-search"));
+            clamp.id = clamp.id.concat("-" + i);
+            arr.push(clamp);
+        }
+    } else {
+        var cable = cloneTemplate(document.getElementById("template-cable-search"));
+        arr.push(cable);
+        for (var i = 1; i <= 2; i++) {
+            var ferr = cloneTemplate(document.getElementById("template-ferrule-search"));
+            ferr.id = ferr.id.concat('-' + i);
+            arr.push(ferr);
+            var clamp = cloneTemplate(document.getElementById("template-clamp-search"));
+            clamp.id = clamp.id.concat('-' + i);
+            arr.push(clamp);
+        }
+    }
+    return arr;
+}
+
+function next(e) {
+    e.nextElementSibling.classList.remove("hide");
+    e.classList.add("hide");
+}
+
+function back(e) {
+    e.previousElementSibling.classList.remove("hide");
+    e.classList.add("hide");
 }
