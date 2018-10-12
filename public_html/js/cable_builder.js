57a2b90de6151373c472e2a06f5a57cf93edf408
Spitfyre03 <spitfyre03@gmail.com>
2018-10-12 19:45:53 -0400

    Check truthiness when getting elements by ID
    When getting elements from document by ID, validate before calling

--- public_html/js/cable_builder.js
+++ public_html/js/cable_builder.js
@@ -1,94 +1,115 @@
 
 function cloneTemplate(element) {
-    var clone = element.cloneNode(true);
-    var index = cookieArrayGet("cable");
-    clone.setAttribute("id", element.id.replace("template-","").concat("-" + index).trim());
+    var clone = null;
+    if (element) {
+        clone = element.cloneNode(true);
+        clone.setAttribute("id", clone.id.replace("template-","").trim());
+        var tbody = document.getElementById("quote-items-body");
+        if (tbody) {
+            var index = tbody.children.length - 1;
+            clone.setAttribute("id", clone.id.concat("-" + index).trim());
+        }
+    }
     return clone;
 }
 
 function startCable() {
-    if (addCable()) {
-        var clone = cloneTemplate(document.getElementById("template-cable-setup"));
-        document.getElementById("new-cable").appendChild(clone);
-        document.getElementById("cables-form").classList.remove("hide");
+    var template = document.getElementById("template-cable-setup");
+    var form = document.getElementById("cables-form");
+    var container;
+    if (addCable() && (container = document.getElementById("new-cable")) && template && form) {// assignment intended
+        var clone = cloneTemplate(template);
+        container.appendChild(clone);
+        form.classList.remove("hide");
     }
 }
 
 function addCable() {
-    var body = document.getElementById("quote-items-body");
-    var index = body.rows.length;
     var div = document.getElementById("new-cable");
-    if (div != null) {
+    if (div) {
         alert("You must complete the current cable first");
         return false;
     }
-    var display = document.createElement("div");
-    display.id = "new-cable";
-    document.getElementById("form-panel").appendChild(display);
-    var cell = document.createElement("td");
-    cell.innerText = "Cable " + index;
-    var row = document.createElement("tr");
-    row.id = "quote-item-" + index;
-    row.appendChild(cell);
+    var body = document.getElementById("quote-items-body");
+    var panel = document.getElementById("form-panel");
     var bRow = document.getElementById("table-cable-adder");
-    bRow.insertAdjacentElement("beforebegin",row);
-    return true;
+    if (body && panel && bRow) {
+        var index = body.rows.length;
+        var display = document.createElement("div");
+        display.id = "new-cable";
+        panel.appendChild(display);
+        var cell = document.createElement("td");
+        cell.innerHTML = "<h4>Cable " + index + "</h4>";
+        var row = document.createElement("tr");
+        row.id = "quote-item-" + index;
+        row.appendChild(cell);
+        bRow.insertAdjacentElement("beforebegin",row);
+        return true;
+    }
 }
 
 function setupCable(container) {
-    var type = container.querySelector("select").value;
-    createCableTree(type);
-    var parent = container.parentElement;
-    if (parent.children.length > 1) {
-        parent.innerHTML = '';
-        parent.appendChild(container);
+    if (container) {
+        var type = container.querySelector("select").value;
+        createCableTree(type);
+        var parent = container.parentElement;
+        if (parent.children.length > 1) {
+            parent.innerHTML = '';
+            parent.appendChild(container);
+        }
+        setupCableForm(type).forEach(function(child) {
+            parent.appendChild(child);
+        });
+        next(container);
     }
-    setupCableForm(type).forEach(function(child) {
-        parent.appendChild(child);
-    });
-    next(container);
 }
 
 /** Initiates a tree for displaying the cable in the review table */
 function createCableTree(type) {
-    var isCluster = type > 1;
-    var itt = (isCluster ? 4 : 1);
-    var index = document.getElementById("quote-items-body").getElementsByTagName("tr").length - 1;
-    var cell = document.getElementById("quote-item-" + index).children[index - 1];
-    if (cell.querySelector("ul") !== null) {
-        cell.removeChild(cell.lastChild);
-    }
-    var tree = document.createElement("ul");
-    var id = 'a';
-    var num = 1;
-    for (var x = 0; x < itt; x++) {
-        var cable = document.createElement("li");
-        cable.id = "cable-" + id;
-        cable.classList.add("hide");
-        for (var y = 0; y < 2; y++) {  
-            var ferrList = document.createElement("ul");
-            var ferr = document.createElement("li");
-            ferr.id = "ferrule-" + num;
-            ferr.classList.add("hide");
-            var cabList = document.createElement("ul");
-            var shrink = document.createElement("li");
-            shrink.innerText = "Shrink";
-            cabList.appendChild(shrink);
-            if (isCluster && y != 1) {
-                var clamp = document.createElement("li");
-                clamp.id = "clamp-" + num;
-                clamp.classList.add("hide");
-                cabList.appendChild(clamp);
+    var tableBody = document.getElementById("quote-items-body");
+    if (tableBody && tableBody.getElementsByTagName("tr")) {
+        var index = tableBody.getElementsByTagName("tr").length - 1;
+        var itemRow = tableBody.children[index - 1];
+        if (itemRow) {
+            var isCluster = type > 1;
+            var itt = (isCluster ? 4 : 1);
+            var cell = itemRow.firstElementChild;
+            if (cell.querySelector("ul")) {
+                cell.removeChild(cell.lastChild);
             }
-            ferr.appendChild(cabList);
-            ferrList.appendChild(ferr);
-            cable.appendChild(ferrList);
-            num++;
+            var tree = document.createElement("ul");
+            var id = 'a';
+            var num = 1;
+            for (var x = 0; x < itt; x++) {
+                var cable = document.createElement("li");
+                cable.id = "cable-" + id;
+                cable.classList.add("hide");
+                var ferrList = document.createElement("ul");
+                for (var y = 0; y < 2; y++) {
+                    var ferr = document.createElement("li");
+                    ferr.id = "ferrule-" + num;
+                    ferr.classList.add("hide");
+                    var cabList = document.createElement("ul");
+                    var shrink = document.createElement("li");
+                    shrink.innerText = "Shrink";
+                    cabList.appendChild(shrink);
+                    if (isCluster && y != 1) {
+                        var clamp = document.createElement("li");
+                        clamp.id = "clamp-" + num;
+                        clamp.classList.add("hide");
+                        cabList.appendChild(clamp);
+                    }
+                    ferr.appendChild(cabList);
+                    ferrList.appendChild(ferr);
+                    cable.appendChild(ferrList);
+                    num++;
+                }
+                id = String.fromCharCode(id.charCodeAt() + 1);
+                tree.appendChild(cable);
+            }
+            cell.appendChild(tree);
         }
-        id = String.fromCharCode(id.charCodeAt() + 1);
-        tree.appendChild(cable);
     }
-    cell.appendChild(tree);
 }
 
 function setupCableForm(type) {
